8d8e04abf6c7a04e8bbcc3a1b6b92dd96be79da4
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-22 00:13:09 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    template: '<App/>',
+    components: {App}
 })
