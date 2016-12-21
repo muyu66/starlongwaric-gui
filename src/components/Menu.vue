1fbe60e995ead073cd63e236e70256067cf48a8f
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-22 00:19:04 +0800

    fix

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="list-inline">
+    <ul class="list-inline" id="menu">
         <li>
             <button type="button" class="btn btn-default btn-lg">我的舰队</button>
         </li>
