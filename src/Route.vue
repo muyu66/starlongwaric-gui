b0c7bb5554a83f9e607e15290358c9cf183be2db
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-24 00:52:27 +0800

    fix

--- src/Route.vue
+++ src/Route.vue
@@ -1,5 +1,10 @@
 <template>
     <div id="route">
+        <h1>Hello App!</h1>
+        <p>
+            <router-link to="/foo">Go to Foo</router-link>
+            <router-link to="/bar">Go to Bar</router-link>
+        </p>
         <router-view></router-view>
     </div>
 </template>
