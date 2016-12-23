39e5c71e70c012ea971e74f67ce6d02de7d7335e
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-23 21:32:23 +0800

    按钮样式调整

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,13 +1,13 @@
 <template>
     <ul class="list-inline" id="topbar">
         <li>
-            <button type="button" class="btn btn-default btn-lg">舰队</button>
+            <button type="button" class="btn btn-default btn-lg menu">舰队</button>
         </li>
         <li>
-            <button type="button" class="btn btn-default btn-lg">舰队</button>
+            <button type="button" class="btn btn-default btn-lg menu">舰队</button>
         </li>
         <li>
-            <button type="button" class="btn btn-default btn-lg">舰队</button>
+            <button type="button" class="btn btn-default btn-lg menu">舰队</button>
         </li>
     </ul>
 </template>
@@ -19,5 +19,15 @@
 </script>
 
 <style scoped>
+    .menu {
+        width: 50%;
+        height: 50%;
+        background-color: rgba(0, 127, 127, 0.313726);
+        transform: translate3d(-50%, -50%, 0px) matrix3d(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, -800, 400, 2000, 1);
+    }
 
+    .menu:hover {
+        box-shadow: 0 0 12px rgba(0, 255, 255, 0.75);
+        border: 1px solid rgba(127, 255, 255, 0.75);
+    }
 </style>
