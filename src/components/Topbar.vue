96e120fe659aa0ec6a6c43c25f847ed519435404
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-23 21:46:25 +0800

    fix

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,30 +1,24 @@
 <template>
-    <ul class="list-inline" id="topbar">
-        <li>
-            <div class="element"
-                 style="background-color: rgba(0, 127, 127, 0.298039); position: absolute; transform: translate3d(-50%, -50%, 0px) matrix3d(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, -400, 400, 1000, 1);">
-                <div class="number">82</div>
-                <div class="symbol">Pb</div>
-                <div class="details">Lead<br>207.2</div>
-            </div>
-        </li>
-        <li>
-            <div class="element"
-                 style="background-color: rgba(0, 127, 127, 0.298039); position: absolute; transform: translate3d(-50%, -50%, 0px) matrix3d(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, -400, 400, 1000, 1);">
-                <div class="number">82</div>
-                <div class="symbol">Pb</div>
-                <div class="details">Lead<br>207.2</div>
-            </div>
-        </li>
-        <li>
-            <div class="element"
-                 style="background-color: rgba(0, 127, 127, 0.298039); position: absolute; transform: translate3d(-50%, -50%, 0px) matrix3d(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, -400, 400, 1000, 1);">
-                <div class="number">82</div>
-                <div class="symbol">Pb</div>
-                <div class="details">Lead<br>207.2</div>
-            </div>
-        </li>
-    </ul>
+    <div id="topbar">
+        <div class="element"
+             style="background-color: rgba(0, 127, 127, 0.298039); position: absolute; transform: translate3d(-50%, -50%, 0px) matrix3d(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, -400, 400, 1000, 1);">
+            <div class="number">82</div>
+            <div class="symbol">Pb</div>
+            <div class="details">Lead<br>207.2</div>
+        </div>
+        <div class="element"
+             style="background-color: rgba(0, 127, 127, 0.298039); position: absolute; transform: translate3d(-50%, -50%, 0px) matrix3d(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, -400, 400, 1000, 1);">
+            <div class="number">82</div>
+            <div class="symbol">Pb</div>
+            <div class="details">Lead<br>207.2</div>
+        </div>
+        <div class="element"
+             style="background-color: rgba(0, 127, 127, 0.298039); position: absolute; transform: translate3d(-50%, -50%, 0px) matrix3d(1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, -400, 400, 1000, 1);">
+            <div class="number">82</div>
+            <div class="symbol">Pb</div>
+            <div class="details">Lead<br>207.2</div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -34,5 +28,17 @@
 </script>
 
 <style scoped>
+    .element:hover {
+        box-shadow: 0 0 12px rgba(0, 255, 255, 0.75);
+        border: 1px solid rgba(127, 255, 255, 0.75);
+    }
 
+    .element {
+        width: 120px;
+        height: 160px;
+        box-shadow: 0 0 12px rgba(0, 255, 255, 0.5);
+        border: 1px solid rgba(127, 255, 255, 0.25);
+        text-align: center;
+        cursor: default;
+    }
 </style>
