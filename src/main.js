1cdc702211b50c0c3d19fb4262fea8efd563f37c
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-24 23:49:24 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
+Vue.use(VueRouter)
+import Index from './pages/Index'
+import Login from './pages/Login'
+
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/index',
+            component: Index
+        },
+    ]
+})
 new Vue({
-    el: '#app',
-    template: '<App/>',
-    components: {App},
+    router: router,
+    render: h => h(App)
 }).$mount('#app')
