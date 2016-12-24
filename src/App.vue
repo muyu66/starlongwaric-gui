52849501dafb7bc33233a77253f52ebb4782fba0
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-24 23:29:11 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
 </template>
 
 <script>
+    import VueRouter from 'vue-router'
+
+    Vue.use(VueRouter)
     const Foo = {template: '<div>foo</div>'}
     const Bar = {template: '<div>bar</div>'}
 
