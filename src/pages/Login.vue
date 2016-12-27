640069997ce9ee15a1140f8dfebe43674836eed5
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-28 00:44:55 +0800

    fix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -11,18 +11,25 @@
                 google_code: '',
             }
         },
+        render: function () {
+            this.$http.get('//127.0.0.1:10000/auth/code').then((response) => {
+                console.log(response);
+                this.google_code = response.body
+            }, (response) => {
+            });
+        },
         created: function () {
             this.getAuthCode();
         },
-        methods: {
-            getAuthCode: function () {
-                this.$http.get('//127.0.0.1:10000/auth/code').then((response) => {
-                    console.log(response);
-                    this.google_code = response.body
-                }, (response) => {
-                });
-            }
-        },
+//        methods: {
+//            getAuthCode: function () {
+//                this.$http.get('//127.0.0.1:10000/auth/code').then((response) => {
+//                    console.log(response);
+//                    this.google_code = response.body
+//                }, (response) => {
+//                });
+//            }
+//        },
     }
 </script>
 
