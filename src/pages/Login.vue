7e0582a91de0ab57634afcb5416cedb4417fae4c
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-27 22:00:51 +0800

    fix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,18 +1,26 @@
 <template>
     <div id="login">
         登录页
-        <div>{{ getAuthCode() }}</div>
+        <div v-html="google_code"></div>
     </div>
 </template>
 
 <script>
     export default {
         name: 'login',
+        data () {
+            return {
+                google_code: '',
+            }
+        },
+        created: function () {
+            this.getAuthCode();
+        },
         methods: {
             getAuthCode: function () {
                 this.$http.get('//127.0.0.1:10000/auth/code').then((response) => {
                     console.log(response);
-                    return response;
+                    this.google_code = response;
                 }, (response) => {
                 });
             }
