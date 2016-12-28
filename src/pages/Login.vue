598c80b0abb6d2735261bde778472ab92f74cbba
Zhou Yu <muyu.zhouyu@outlook.com>
2016-12-28 10:04:53 +0800

    fix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,19 @@
 <template>
     <div id="login" v-html="google_code">
+        <aaaa></aaaa>
     </div>
 </template>
 
 <script>
+    Vue.component('aaaa', {
+        render: function () {
+            this.$http.get('//127.0.0.1:10000/auth/code').then((response) => {
+                console.log(response);
+                this.google_code = response.body
+            }, (response) => {
+            });
+        },
+    });
     export default {
         name: 'login',
         data () {
@@ -11,25 +21,18 @@
                 google_code: '',
             }
         },
-//        render: function () {
-//            this.$http.get('//127.0.0.1:10000/auth/code').then((response) => {
-//                console.log(response);
-//                this.google_code = response.body
-//            }, (response) => {
-//            });
-//        },
         created: function () {
             this.getAuthCode();
         },
-        methods: {
-            getAuthCode: function () {
-                this.$http.get('//127.0.0.1:10000/auth/code').then((response) => {
-                    console.log(response);
-                    this.google_code = response.body
-                }, (response) => {
-                });
-            }
-        },
+//        methods: {
+//            getAuthCode: function () {
+//                this.$http.get('//127.0.0.1:10000/auth/code').then((response) => {
+//                    console.log(response);
+//                    this.google_code = response.body
+//                }, (response) => {
+//                });
+//            }
+//        },
     }
 </script>
 
