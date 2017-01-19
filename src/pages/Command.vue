f33754a74bbf1bc900744b198f899cad85fcbaef
Zhou Yu <muyu.zhouyu@outlook.com>
2017-01-19 18:56:42 +0800

    战斗风格 新增, icon 新增

--- src/pages/Command.vue
+++ src/pages/Command.vue
@@ -1,26 +1,15 @@
 <template>
     <div id="command">
         <div class="panel panel-default">
-            <div class="panel-heading">指定作战计划</div>
-            <div class="panel-body">
-                <div class="list-group">
-                    <a class="list-group-item" v-for="item in fight_plan">
-                        <h4 class="list-group-item-heading">{{ item.text }}</h4>
-                        <p class="list-group-item-text">{{ item.desc }}</p>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
             <div class="panel-heading">任命指挥官</div>
             <div class="panel-body">
                 <div class="list-group">
                     <a class="list-group-item" v-for="item in my_fleet_staff"
-                       v-on:click="postStaffAppointCommander(item.id,item.is_commander)">
+                       @click="postStaffAppointCommander(item.id,item.is_commander)">
                         <h4 class="list-group-item-heading">
                             {{ item.name }}
                             <span class="label label-primary" v-if="item.is_commander">
-                                指挥官
+                                当前指挥官
                             </span>
                         </h4>
                         <p class="list-group-item-text">
@@ -34,12 +23,14 @@
             </div>
         </div>
         <div class="panel panel-default">
-            <div class="panel-heading">指定行动计划</div>
+            <div class="panel-heading">命令指挥官</div>
             <div class="panel-body">
                 <div class="list-group">
-                    <a class="list-group-item" v-for="action_plan in action_plans">
-                        <h4 class="list-group-item-heading">{{ action_plan.text }}</h4>
-                        <p class="list-group-item-text">{{ action_plan.desc }}</p>
+                    <a :class="{ active: displayCommanderStyle(item.id) }" class="list-group-item"
+                       v-for="item in action_plans"
+                       @click="postFleetConfigCommanderStyle(item.id)">
+                        <h4 class="list-group-item-heading">{{ item.text }}</h4>
+                        <p class="list-group-item-text">{{ item.desc }}</p>
                     </a>
                 </div>
             </div>
@@ -54,32 +45,17 @@
             return {
                 my_body: '',
                 my_fleet_staff: '',
+                commander_style: '',
                 action_plans: [
                     {
+                        id: '1',
                         text: '优先 战斗',
-                        desc: '主动寻求战机',
+                        desc: '对于战斗事件，主动应战',
                     },
                     {
+                        id: '2',
                         text: '优先 躲避',
                         desc: '尽量避免冲突',
-                    },
-                    {
-                        text: '疯狂屠戮',
-                        desc: '无视敌友，无差别的疯狂攻击',
-                    }
-                ],
-                fight_plan: [
-                    {
-                        text: '注重 防御',
-                        desc: '使用能量盾构建防御墙',
-                    },
-                    {
-                        text: '注重 破盾',
-                        desc: '注重 破盾',
-                    },
-                    {
-                        text: '注重 攻击',
-                        desc: '注重 攻击',
                     },
                 ],
                 jobs: [
@@ -90,6 +66,7 @@
         created: function () {
             this.getBody();
             this.getFleetStaff();
+            this.getFleetConfigCommanderStyle();
         },
         methods: {
             getBody: function () {
@@ -117,12 +94,40 @@
                 }
 
                 this.$http.post(
-                    'http://www.slw.app/staff/appoint-commander', {commander_id: id}, window.auth_header
+                    'http://www.slw.app/staff/appoint-commander', { commander_id: id }, window.auth_header
                 ).then((response) => {
                     this.getFleetStaff();
                 }, (response) => {
                     console.log(response);
                 });
+            },
+            getFleetConfigCommanderStyle: function () {
+                this.$http.get(
+                    'http://www.slw.app/fleet_configs/commander_style', window.auth_header
+                ).then((response) => {
+                    this.commander_style = response.body.commander_style;
+                }, (response) => {
+                    console.log(response);
+                });
+            },
+            postFleetConfigCommanderStyle: function (style) {
+                this.$http.post(
+                    'http://www.slw.app/fleet_configs/commander_style', { style: style }, window.auth_header
+                ).then((response) => {
+                    if (response.body.code == 422) {
+                        alert(response.body.msg);
+                    } else {
+                        this.getFleetConfigCommanderStyle();
+                    }
+                }, (response) => {
+                    console.log(response);
+                });
+            },
+            displayCommanderStyle: function (id) {
+                if (id === this.commander_style) {
+                    return true;
+                }
+                return false;
             },
         },
     }
